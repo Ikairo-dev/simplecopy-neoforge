2e6cb7fc978e8ce2935540e595b75303b63ee66d
kairo <kairo@kairo.com>
2025-09-18 10:36:02 -0500

    remove unnecessary file from git and refactor code

--- src/main/java/com/ikairo/SimpleCopy.java
+++ src/main/java/com/ikairo/SimpleCopy.java
@@ -2,11 +2,11 @@ package com.ikairo;
 
 import net.neoforged.fml.common.Mod;
 
-@Mod(ExampleMod.MOD_ID)
-public final class ExampleMod {
+@Mod(SimpleCopy.MOD_ID)
+public final class SimpleCopy {
     public static final String MOD_ID = "simplecopy";
 
-    public ExampleMod() {
+    public SimpleCopy() {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
         // However, some things (like registries and resources) may still be uninitialized.
         // Proceed with mild caution.
